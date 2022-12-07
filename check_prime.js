58b7972210448cdda6a13ae62b098b81e649ac85
bhaskarrajput51 <bhaskarsinghrajput51@gmail.com>
2022-12-07 09:45:08 +0530

    comment

--- check_prime.js
+++ check_prime.js
@@ -1,9 +1,9 @@
 let num=31;
 let count=0;
-for(let i=0;i<=num;i++){
-    if(num%i>=0){
+for(let i=1;i<=num;i++){
+    if(num%i==0){
         count++;
-    }if(count<=2){
+    }if(count==2){
         console.log(num,"is prime");
     }else{
         console.log(num,"is not a prime");
